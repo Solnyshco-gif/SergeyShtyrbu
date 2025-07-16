ae646a3bfb5b00097f73d9100131922b7a5e2d17
Solnyshco-gif <shtyrbus@yandex.ru>
2025-07-16 14:59:11 +0300

    Начальный коммит для перезаливки practical_task_3

---
+++ src/main/java/ru/SergeyShtyrbu/tgBot/TgBotApplication.java
@@ -0,0 +1,14 @@
+package ru.SergeyShtyrbu.tgBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+@EntityScan(basePackages = "ru.SergeyShtyrbu.tgBot")
+public class TgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgBotApplication.class, args);
+	}
+}
