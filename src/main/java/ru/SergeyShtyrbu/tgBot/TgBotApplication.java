7d59a3954566794d2faca822e54baa98dd1aedeb
Solnyshco-gif <shtyrbus@yandex.ru>
2025-07-16 21:26:40 +0300

    Обновил main: добавлена логика для задания 4

--- src/main/java/ru/SergeyShtyrbu/tgBot/TgBotApplication.java
+++ src/main/java/ru/SergeyShtyrbu/tgBot/TgBotApplication.java
@@ -1,14 +1,15 @@
 package ru.SergeyShtyrbu.tgBot;
 
+import com.fasterxml.jackson.annotation.JsonTypeId;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("ru.SergeyShtyrbu.tgBot.entity")
 public class TgBotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TgBotApplication.class, args);
 	}
+
 }
